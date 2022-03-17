6a2662edd24104e1bed8a004a4d6e53683c9b7a9
Anatol Ulrich <anatol@versteht.es>
2022-03-17 03:12:45 +0100

    fix u8 -> T, make Vec traits public, add docs, add horrible impl required by ts-rs

--- src/traits/vec.rs
+++ src/traits/vec.rs
@@ -3,15 +3,42 @@ use core::{
     ops::{Deref, DerefMut},
     slice,
 };
+use std::marker::PhantomData;
 
 use serde::Serialize;
 
-trait Collection<T> {
+pub trait Collection<T> {
     type Iter<'iter>: Iterator<Item = &'iter T>
     where
         Self: 'iter,
         T: 'iter;
     fn iterate<'iter>(&'iter self) -> Self::Iter<'iter>;
+}
+
+pub struct NothingIterator<'it> {
+    _phantom: PhantomData<&'it ()>,
+}
+
+impl<'it> NothingIterator<'it> {
+    pub fn new() -> Self {
+        Self {
+            _phantom: PhantomData::default(),
+        }
+    }
+}
+impl<'it> Iterator for NothingIterator<'it> {
+    type Item = &'it ();
+
+    fn next(&mut self) -> Option<Self::Item> {
+        None
+    }
+}
+impl Collection<()> for () {
+    type Iter<'iter> = NothingIterator<'iter>;
+
+    fn iterate<'iter>(&'iter self) -> Self::Iter<'iter> {
+        NothingIterator::new()
+    }
 }
 
 #[cfg(feature = "use-std")]
@@ -36,7 +63,7 @@ impl<T, const N: usize> Collection<T> for heapless::Vec<T, N> {
     type Iter<'iter> = slice::Iter<'iter, T> where T: 'iter;
 }
 
-trait IterMut<T> {
+pub trait IterMut<T> {
     type IterMut<'iter>: Iterator<Item = &'iter mut T>
     where
         Self: 'iter,
@@ -44,7 +71,7 @@ trait IterMut<T> {
     fn iterate_mut<'iter>(&'iter mut self) -> Self::IterMut<'iter>;
 }
 
-trait CollectionMut<T>: IterMut<T> {
+pub trait CollectionMut<T>: IterMut<T> {
     type Err;
     fn push(&mut self, value: T) -> Result<(), Self::Err>;
 }
@@ -92,11 +119,13 @@ impl<T, const N: usize> CollectionMut<T> for heapless::Vec<T, N> {
     }
 }
 
-trait PostcardVec<T>: Collection<T> + Serialize + Deref + AsRef<[u8]> {}
-impl<T, C: Collection<T> + Serialize + Deref + AsRef<[u8]>> PostcardVec<T> for C {}
+/// a serializable vector-like
+pub trait PostcardVec<T>: Collection<T> + Serialize + Deref + AsRef<[T]> {}
+impl<T, C: Collection<T> + Serialize + Deref + AsRef<[T]>> PostcardVec<T> for C {}
 
-trait PostcardVecMut<T>: PostcardVec<T> + CollectionMut<T> + DerefMut + AsMut<[u8]> {}
-impl<T, C: PostcardVec<T> + CollectionMut<T> + DerefMut + AsMut<[u8]>> PostcardVecMut<T> for C {}
+/// a serializable and mutable vector-like
+pub trait PostcardVecMut<T>: PostcardVec<T> + CollectionMut<T> + DerefMut + AsMut<[T]> {}
+impl<T, C: PostcardVec<T> + CollectionMut<T> + DerefMut + AsMut<[T]>> PostcardVecMut<T> for C {}
 
 #[cfg(all(test, feature = "use-std"))]
 mod tests {
@@ -106,27 +135,27 @@ mod tests {
     use super::*;
 
     #[derive(Serialize, Deserialize)]
-    struct Clip<T, C: PostcardVecMut<T>> {
-        beats: C,
+    struct Outer<T, C: PostcardVecMut<T>> {
+        content: C,
         _phantom: T,
     }
     #[test]
     fn it_works() {
         type HV32<T> = heapless::Vec<T, 32>;
 
-        let mut std = Clip {
-            beats: vec![1, 2, 3],
+        let mut std = Outer {
+            content: vec![1, 2, 3],
             _phantom: Default::default(),
         };
-        std.beats.push(5);
+        std.content.push(5);
 
-        let _data: &[u8] = &std.beats;
+        let _data: &[u8] = &std.content;
 
-        let mut heapless = Clip {
-            beats: HV32::from_slice(&[1, 2, 3]).unwrap(),
+        let mut heapless = Outer {
+            content: HV32::from_slice(&[1, 2, 3]).unwrap(),
             _phantom: Default::default(),
         };
-        heapless.beats.push(5).unwrap();
+        heapless.content.push(5).unwrap();
 
         let ser_std = serde_json::to_string(&std).unwrap();
         let ser_heapless = serde_json::to_string(&heapless).unwrap();
@@ -134,7 +163,7 @@ mod tests {
         assert_eq!(ser_std, ser_heapless);
 
         // the LHS/RHS swap is intentional
-        let _de_std: Clip<u8, std::vec::Vec<u8>> = serde_json::from_str(&ser_heapless).unwrap();
-        let _de_heapless: Clip<u8, HV32<_>> = serde_json::from_str(&ser_std).unwrap();
+        let _de_std: Outer<u8, std::vec::Vec<u8>> = serde_json::from_str(&ser_heapless).unwrap();
+        let _de_heapless: Outer<u8, HV32<_>> = serde_json::from_str(&ser_std).unwrap();
     }
 }
